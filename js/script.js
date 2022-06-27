2c3865c55950005280805055c2207e10f2781f49
Lera-Iv <103205215+Lera-Iv@users.noreply.github.com>
2022-06-27 23:35:58 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// console.log('Ура! Заработало!!!');
+
+let iconMenu = document.querySelector('.icon');
+let menuBody = document.querySelector('.menu');
+
+if (iconMenu){
+    iconMenu.addEventListener('click', function (e){
+        iconMenu.classList.toggle('active');
+        menuBody.classList.toggle('active');
+    });
+}
+
+
+$(function() {
+   
+})
